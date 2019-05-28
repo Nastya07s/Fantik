31bbffeb2a79561d464a74b37267f72f6247f5a1
Nastya07s <44784575+Nastya07s@users.noreply.github.com>
2019-05-28 20:26:04 +0300

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { ArticleComponent } from './article/article.component';
+
+const appRoutes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'name', component: ArticleComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { ArticleComponent } from './article/article.component';
     ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
